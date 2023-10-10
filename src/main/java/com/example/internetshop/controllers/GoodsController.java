87d14a89541fc10b9dd6c5597ec8334ee1afd7ba
BD <VED-Intlaw@yandex.ru>
2023-10-10 14:26:13 +0300

    Created controller with methods to create, get and delete goods.

---
+++ src/main/java/com/example/internetshop/controllers/GoodsController.java
@@ -0,0 +1,68 @@
+package com.example.internetshop.controllers;
+import com.example.internetshop.models.Goods;
+import com.example.internetshop.repositories.GoodsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+public class GoodsController {
+
+    GoodsRepository goodsRepository;
+
+    public GoodsController(GoodsRepository goodsRepository) {
+        this.goodsRepository = goodsRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addGoods(@RequestBody Goods product) {
+        int productId = product.getGoodID();
+        Optional<Goods> byId = goodsRepository.findById(productId);
+        if (byId.isPresent()) {
+            return new ResponseEntity<>("Product is already exist",HttpStatus.BAD_REQUEST);
+        }
+        goodsRepository.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllGoods () {
+        List<Goods> listOfProducts = new ArrayList<>();
+        for (Goods product : goodsRepository.findAll()) {
+            listOfProducts.add(product);
+        }
+        return new ResponseEntity<>(listOfProducts,HttpStatus.OK);
+    }
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<?> getProductByID (@PathVariable int goodID) {
+        if (! goodsRepository.findById(goodID).isPresent()) {
+            return new ResponseEntity<>
+                    ("This product is abcent",HttpStatus.NOT_FOUND);
+        }
+        Goods requestedProduct = goodsRepository.findById(goodID).get();
+        return new ResponseEntity<>(requestedProduct,HttpStatus.OK);
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<?> deleteGoods (@PathVariable int goodID) {
+        if (! goodsRepository.findById(goodID).isPresent()) {
+            return new ResponseEntity<>
+                    ("This product is abcent",HttpStatus.BAD_REQUEST);
+        }
+        goodsRepository.delete(goodsRepository.findById(goodID).get());
+        return new ResponseEntity<>("Successfully deleted",HttpStatus.OK);
+    }
+
+
+
+
+
+
+
+}
