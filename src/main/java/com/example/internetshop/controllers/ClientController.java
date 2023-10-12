d08a17bf5790c349048a6bba505af314bc213ed5
BD <VED-Intlaw@yandex.ru>
2023-10-12 11:28:19 +0300

    Created methods for adding, getting and deleting clients

---
+++ src/main/java/com/example/internetshop/controllers/ClientController.java
@@ -0,0 +1,64 @@
+package com.example.internetshop.controllers;
+import com.example.internetshop.models.Booking;
+import com.example.internetshop.models.Client;
+import com.example.internetshop.repositories.ClientRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addClient (@RequestBody Client client) {
+        int inn = client.getClientINN();
+        Optional<Client> byINN = clientRepository.findById(inn);
+        if (byINN.isPresent()) {
+            return new ResponseEntity<>
+            ("Client is already exist", HttpStatus.BAD_REQUEST);
+        }
+        clientRepository.save(client);
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllClients () {
+        List<Client> clients = new ArrayList<>();
+        for (Client client : clientRepository.findAll()) {
+            clients.add(client);
+        }
+        return new ResponseEntity<>(clients,HttpStatus.OK);
+    }
+
+    @GetMapping("/{inn}")
+    public ResponseEntity<?> getClientByINN (int clientINN) {
+        if (clientRepository.findById(clientINN).isEmpty()) {
+            return new ResponseEntity<>
+            ("Client with such INN doesn't exist",HttpStatus.NOT_FOUND);
+        }
+        Client requestedClient = clientRepository.findById(clientINN).get();
+        return new ResponseEntity<>(requestedClient,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{inn}")
+    public ResponseEntity<?> deleteClientByINN (int clientINN) {
+        if (clientRepository.findById(clientINN).isEmpty()) {
+            return new ResponseEntity<>
+            ("Client with such INN doesn't exist",HttpStatus.NOT_FOUND);
+        }
+        clientRepository.delete(clientRepository.findById(clientINN).get());
+        return new ResponseEntity<>("Deleted successfully",HttpStatus.OK);
+    }
+
+}
