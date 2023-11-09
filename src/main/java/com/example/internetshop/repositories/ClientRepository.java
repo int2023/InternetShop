8375ade795496d888e51adbed09d01f80f58c2d0
BD <VED-Intlaw@yandex.ru>
2023-11-09 10:42:37 +0300

    corrected parameters in interfaces

--- src/main/java/com/example/internetshop/repositories/ClientRepository.java
+++ src/main/java/com/example/internetshop/repositories/ClientRepository.java
@@ -1,6 +1,7 @@
 package com.example.internetshop.repositories;
 
+import com.example.internetshop.models.Client;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ClientRepository extends CrudRepository<Integer,Integer> {
+public interface ClientRepository extends CrudRepository<Client,Integer> {
 }
