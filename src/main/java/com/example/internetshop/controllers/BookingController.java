badfd351e90b9ee8e5a1b0d11d4895f6d1931714
BD <VED-Intlaw@yandex.ru>
2023-12-07 11:55:09 +0300

    Added new autowired annotations, and made changes in method addBooking for setting changes in OrderPosition and Booking

--- src/main/java/com/example/internetshop/controllers/BookingController.java
+++ src/main/java/com/example/internetshop/controllers/BookingController.java
@@ -1,7 +1,13 @@
 package com.example.internetshop.controllers;
 import com.example.internetshop.models.Booking;
+import com.example.internetshop.models.Client;
 import com.example.internetshop.models.Goods;
+import com.example.internetshop.models.OrderPosition;
 import com.example.internetshop.repositories.BookingsRepository;
+import com.example.internetshop.repositories.ClientRepository;
+import com.example.internetshop.repositories.GoodsRepository;
+import com.example.internetshop.repositories.OrderPositionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +19,38 @@ import java.util.Optional;
 @RequestMapping("/bookings")
 public class BookingController {
     BookingsRepository bookingsRepository;
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    GoodsRepository goodsRepository;
+    @Autowired
+    OrderPositionsRepository orderPositionsRepository;
     public BookingController(BookingsRepository bookingsRepository) {
         this.bookingsRepository = bookingsRepository;
     }
     @PostMapping
     public ResponseEntity<?> addBooking (@RequestBody Booking booking) {
-        int bookingNumber = booking.getNumber();
-        Optional<Booking> number = bookingsRepository.findById(bookingNumber);
-        if (number.isPresent()) {
-            return new ResponseEntity<>
-                    ("Booking is already exist", HttpStatus.BAD_REQUEST);
+
+        if (clientRepository.findById(booking.getClient().getClientINN()).isEmpty()) {
+            return new ResponseEntity<>("Client is not registered",
+                    HttpStatus.NOT_FOUND);
         }
-        bookingsRepository.save(booking);
-        return new ResponseEntity<>(booking, HttpStatus.OK);
+        Booking booking1 = new Booking();
+        booking1.setBookingDate(booking.getBookingDate());
+        Optional<Client> client = clientRepository.findById(booking.getClient().getClientINN());
+        booking1.setClient(client.get());
+        bookingsRepository.save(booking1);
+
+        for (OrderPosition position : booking.getPositions()) {
+            OrderPosition orderPosition = new OrderPosition();
+            orderPosition.setBooking(booking1);
+            int goodID = position.getGoods().getGoodID();
+            Optional<Goods> product = goodsRepository.findById(goodID);
+            orderPosition.setGoods(product.get());
+            orderPosition.setGoodsQuantity(position.getGoodsQuantity());
+            orderPositionsRepository.save(orderPosition);
+        }
+       return new ResponseEntity<>("Successfully", HttpStatus.OK);
     }
 
     @GetMapping
@@ -56,6 +81,7 @@ public class BookingController {
         bookingsRepository.delete(bookingsRepository.findById(bookingNumber).get());
         return new ResponseEntity<>("Successfully deleted",HttpStatus.OK);
     }
+
 
 
 }
