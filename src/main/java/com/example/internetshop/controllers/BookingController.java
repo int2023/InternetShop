ec8728e79bae1e2612443abc21b53071f830745d
BD <VED-Intlaw@yandex.ru>
2024-01-16 09:28:14 +0300

    Commented Excel method

--- src/main/java/com/example/internetshop/controllers/BookingController.java
+++ src/main/java/com/example/internetshop/controllers/BookingController.java
@@ -90,54 +90,50 @@ public class BookingController {
     }
 
 
-    @PostMapping("/excel")
-    public ResponseEntity<?> exportExcel() {
-
-        List<Client> clients = new ArrayList<>();
-
-        for (Client client : clientRepository.findAll()) {
-            clients.add(client);
-        }
-        Workbook res = new XSSFWorkbook();
-
-        try (FileOutputStream fileOutputStream = new FileOutputStream(
-                ("C:\\Users\\Bogdan\\Documents\\Bookings.xlsx"))) {
-
-            for (Client c : clients) {
-                Sheet sheet = res.createSheet(c.getClientINN() + "");
-                int index = 1;
-                for (Booking b : c.getBookings()) {
-                    List<OrderPosition> positions = b.getPositions();
-                    int sum = 0;
-                    for (OrderPosition p : positions) {
-                        sum = sum + p.getGoods().getPrice();
-                    }
-                    Row row = sheet.createRow(index);
-                    Cell cellForBookingDate = row.createCell(0);
-                    Cell cellForBookingSum = row.createCell(1);
-                    CellStyle cellStyle = res.createCellStyle();
-                    cellStyle.setDataFormat((short)14);
-                    cellForBookingDate.setCellStyle(cellStyle);
-                    cellForBookingDate.setCellValue(b.getBookingDate());
-                    cellForBookingSum.setCellValue(sum);
-                    index++;
-
-                    // create page in Excel
-                    // for every client - include booking date and
-                    // sum of every booking
-                }
-            }
-            res.write(fileOutputStream);
-            res.close();
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return new ResponseEntity<>("OK", HttpStatus.OK);
-    }
+//    @PostMapping("/excel")
+//    public ResponseEntity<?> exportExcel() {
+//
+//        List<Client> clients = new ArrayList<>();
+//
+//        for (Client client : clientRepository.findAll()) {
+//            clients.add(client);
+//        }
+//        Workbook res = new XSSFWorkbook();
+//
+//        try (FileOutputStream fileOutputStream = new FileOutputStream(
+//                ("C:\\..............."))) {
+//
+//            for (Client c : clients) {
+//                Sheet sheet = res.createSheet(c.getClientINN() + "");
+//                int index = 1;
+//                for (Booking b : c.getBookings()) {
+//                    List<OrderPosition> positions = b.getPositions();
+//                    int sum = 0;
+//                    for (OrderPosition p : positions) {
+//                        sum = sum + p.getGoods().getPrice();
+//                    }
+//                    Row row = sheet.createRow(index);
+//                    Cell cellForBookingDate = row.createCell(0);
+//                    Cell cellForBookingSum = row.createCell(1);
+//                    CellStyle cellStyle = res.createCellStyle();
+//                    cellStyle.setDataFormat((short)14);
+//                    cellForBookingDate.setCellStyle(cellStyle);
+//                    cellForBookingDate.setCellValue(b.getBookingDate());
+//                    cellForBookingSum.setCellValue(sum);
+//                    index++;
+//                }
+//            }
+//            res.write(fileOutputStream);
+//            res.close();
+//
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return new ResponseEntity<>("OK", HttpStatus.OK);
+//    }
 }
 
 
